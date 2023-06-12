5ac3093dc8fba2c41a3f2f2f7355160e9cdb17c4
Cjmars0912 <christianmarsh0912@gmail.com>
2023-06-12 12:43:47 -0400

    inital commit

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: 'recipe-website-43298.firebaseapp.com',
+  projectId: 'recipe-website-43298',
+  storageBucket: 'recipe-website-43298.appspot.com',
+  messagingSenderId: '466908845368',
+  appId: '1:466908845368:web:5ae2072fcfaedcc6df9dcd',
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
